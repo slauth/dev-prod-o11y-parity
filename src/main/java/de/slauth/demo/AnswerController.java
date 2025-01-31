44715f6cc8477bd023772c7c0609916fbb84ada4
slauth <slauth@users.noreply.github.com>
2025-01-31 12:13:50 +0100

    simplify

--- src/main/java/de/slauth/demo/AnswerController.java
+++ src/main/java/de/slauth/demo/AnswerController.java
@@ -3,14 +3,12 @@ package de.slauth.demo;
 import io.micrometer.observation.annotation.Observed;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.slf4j.MDC;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
-import java.util.function.Supplier;
 
 @RestController
 @RequestMapping("/answer")
@@ -24,27 +22,17 @@ public class AnswerController {
             @RequestParam(defaultValue = "The ultimate question of life, the universe, and everything")
             String question
     ) {
-        return withMdc("question", question, () -> {
-            logger.atDebug().setMessage("Going to calculate answer for question: {}").addArgument(question).log();
+        logger.atDebug().setMessage("Going to calculate answer")
+                .addKeyValue("question", question).log();
 
-            int answer = 42;
+        int answer = 42;
 
-            logger.atInfo().setMessage("Answer: {}").addArgument(answer)
-                    .addKeyValue("answer", answer).log();
+        logger.atInfo().setMessage("Answer is ready")
+                .addKeyValue("question", question).addKeyValue("answer", answer).log();
 
-            return Map.of(
-                    "question", question,
-                    "answer", answer
-            );
-        });
-    }
-
-    private static <T> T withMdc(String key, String value, Supplier<T> supplier) {
-        MDC.put(key, value);
-        try {
-            return supplier.get();
-        } finally {
-            MDC.remove(key);
-        }
+        return Map.of(
+                "question", question,
+                "answer", answer
+        );
     }
 }
